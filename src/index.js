c1b519f1a741437f10392112c40420db9e54b822
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-05 11:33:29 -0300

    adding some divs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
+import { apiWeather } from "./env";
+import { apiGif } from "./env";
 
-let myTitle=document.createElement('h1')
-myTitle.innerText="hola gustavito"
-document.body.appendChild(myTitle);
+import './style.css'; 
+
+
+let mainContent=document.createElement('div')
+mainContent.id="mainContent"
+mainContent.className="mainContent";
+
+let myTitle2=document.createElement('p')
+myTitle2.innerText="apiWe: "+apiWeather
+
+let myTitle3=document.createElement('p')
+myTitle3.innerText="ApiGif: "+apiGif
+
+mainContent.appendChild(myTitle2)
+mainContent.appendChild(myTitle3)
+
+let footer=document.createElement('div')
+footer.className="footer";
+footer.id="footer";
+let myFooter=document.createElement('p')
+myFooter.innerText="By Gustavo Bastian(2023)"
+footer.appendChild(myFooter)
+mainContent.appendChild(footer);
+document.body.appendChild(mainContent);
+
+
+
+
+
