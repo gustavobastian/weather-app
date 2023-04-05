321a5529669b2fc69f10f0391c438478c5f5ec1d
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-05 11:07:18 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+let myTitle=document.createElement('h1')
+myTitle.innerText="hola gustavito"
+document.body.appendChild(myTitle);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode:'development', 
+  entry: {
+    index:{
+      import:'./src/index.js',                  
+      },  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Wheather App',
+    }),
+  ],
+  output: {
+    //filename: 'main.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
