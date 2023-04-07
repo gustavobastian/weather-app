8d35f533a8c3ec6198b6191883ae8995f5433c92
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-04-06 21:22:47 -0300

    remove unused weather functions

--- src/weatherService.js
+++ src/weatherService.js
@@ -12,56 +12,10 @@ const weather = function () {
         Data = await (response.json());
         return Data.current;
         
-    }
-    const getTempC = function(){
-        if(this.Data=="noData"){
-            return;
-        }
-        else{
-            return this.Data.temp_c;
-        }
-    }
-    const getTempF = function(){
-        if(this.Data=="noData"){
-            return;
-        }
-        else{
-            return this.Data.temp_F;
-        }
-    }
-    const getHumidity = function(){
-        if(this.Data=="noData"){
-            return;
-        }
-        else{
-            return this.Data.humidity;
-        }
-    }
-
-    const getCloud = function(){
-        if(this.Data=="noData"){
-            return;
-        }
-        else{
-            return this.Data.cloud;
-        }
-    }
-    const getPressureMB = function(){
-        if(this.Data=="noData"){
-            return;
-        }
-        else{
-            return this.Data.pressure_mb;
-        }
-    }
+    }    
 
     return {
-        Data,
-        getTempC,
-        getTempF,
-        getHumidity,
-        getCloud,
-        getPressureMB,
+        Data,        
         getData
     };
 };
